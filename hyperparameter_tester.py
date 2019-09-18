9014de4fed664cdf4b4df1061f55b4863c319912
tomjur <tomj@campus.technion.ac.il>
2019-09-18 19:40:23 +0300

    changes for hyperparameter tester

--- hyperparameter_tester.py
+++ hyperparameter_tester.py
@@ -19,23 +19,27 @@ def _modify_config(config):
     config_copy['general']['save_every_cycles'] = 20000  # don't save model
     config_copy['general']['cycles_per_trajectory_print'] = 20000  # don't save train trajectories
 
-    config_copy['general']['train_episodes_per_cycle'] = random.choice([1000, 500, 100, 50])
+    config_copy['cost']['type'] = random.choice(['linear'])
 
-    config_copy['cost']['collision_cost'] = random.choice([20., 100., 200., 1000.])
-    config_copy['cost']['type'] = random.choice(['huber'])
-    # config_copy['cost']['type'] = random.choice(['linear', 'huber'])
+    config_copy['model']['decrease_learn_rate_if_static_success'] = random.choice([5, 10, 20])
+    config_copy['model']['stop_training_after_learn_rate_decrease'] = random.choice([3, 5, 10])
 
-    config_copy['policy']['learning_rate'] = random.choice([0.001, 0.0001, 0.00001, 0.000001])
-    config_copy['policy']['learning_rate_decrease_rate'] = random.choice([0.1, 0.5, 0.8, 1.0])
+    config_copy['policy']['learning_rate'] = random.choice([0.01, 0.001, 0.0001, ])
+    config_copy['policy']['learning_rate_decrease_rate'] = random.choice([0.95, 0.9, 0.8, 0.5, 0.1])
 
-    layers_size = random.choice([5, 20, 50, 100, 400])
-    number_of_layers = random.choice([2, 3, 4])
+    layers_size = random.choice([2, 5, 10, 20])
+    number_of_layers = random.choice(list(range(2, 9)))
 
     config_copy['policy']['layers'] = [layers_size] * number_of_layers
-    config_copy['policy']['activation'] = random.choice(['relu', 'elu', 'tanh'])
-    config_copy['policy']['base_std'] = random.choice([0.0, 0.05, 0.25, 0.5])
-    config_copy['policy']['log_prob_ignore_pdf'] = random.choice([0.001, 0.00001, 0.0000001])
-    config_copy['policy']['learn_std'] = random.choice([True, False])
+
+    config_copy['value_function']['learning_rate'] = random.choice([0.01, 0.001, 0.0001, ])
+    config_copy['value_function']['learning_rate_decrease_rate'] = random.choice([0.95, 0.9, 0.8, 0.5, 0.1])
+
+    layers_size = random.choice([20, 100, 400])
+    number_of_layers = random.choice(list(range(2, 9)))
+
+    config_copy['value_function']['layers'] = [layers_size] * number_of_layers
+    config_copy['value_function']['max_straight_updates'] = random.choice([10, 100, 200])
 
     return config_copy
 
@@ -63,7 +67,7 @@ if __name__ == '__main__':
     general_log = os.path.join(working_dir, 'general_log_{}.txt'.format(run_name))
 
     # track the best score
-    best_score, best_config = None, None
+    best_cost, best_config = None, None
 
     # while True:
     for group_index in range(number_of_groups):
@@ -74,36 +78,36 @@ if __name__ == '__main__':
         success_rates = []
 
         copy_config(current_config, group_config)
-        final_success_rate = None
+        final_cost = None
         try:
             for try_index in range(tries_per_config):
                 tf.reset_default_graph()
-                current_score = run_for_config(current_config)
-                write_to_text_log(group_log, 'try {} score: {}'.format(try_index, current_score))
-                success_rates.append(current_score)
-                if best_score is not None:
-                    optimistic_scores = [1.] * (tries_per_config - len(success_rates))
-                    optimistic_scores.extend(deepcopy(success_rates))
-                    mean_optimistic_score = np.mean(optimistic_scores)
-                    if mean_optimistic_score < best_score:
-                        write_to_text_log(
-                            group_log, 'aborting, mean optimistic score {} smaller than best score {}'.format(
-                                mean_optimistic_score, best_score))
-                        # no point to test this config further
-                        break
-            final_success_rate = np.mean(success_rates)
-            write_to_text_log(group_log, 'final score: {}'.format(final_success_rate))
+                current_cost = run_for_config(current_config)
+                write_to_text_log(group_log, 'try {} cost: {}'.format(try_index, current_cost))
+                success_rates.append(current_cost)
+                # if best_score is not None:
+                #     optimistic_scores = [1.] * (tries_per_config - len(success_rates))
+                #     optimistic_scores.extend(deepcopy(success_rates))
+                #     mean_optimistic_score = np.mean(optimistic_scores)
+                #     if mean_optimistic_score < best_score:
+                #         write_to_text_log(
+                #             group_log, 'aborting, mean optimistic score {} smaller than best score {}'.format(
+                #                 mean_optimistic_score, best_score))
+                #         # no point to test this config further
+                #         break
+            final_cost = np.mean(success_rates)
+            write_to_text_log(group_log, 'final cost: {}'.format(final_cost))
         except:
             write_to_text_log(group_log, 'error, run threw exception!')
             write_to_text_log(general_log, 'group {} crashed'.format(group_index))
             continue
 
-        if final_success_rate is not None:
+        if final_cost is not None:
             # see if best
-            if best_score is None or final_success_rate > best_score:
-                write_to_text_log(group_log, 'final score is the best')
+            if best_cost is None or final_cost < best_cost:
+                write_to_text_log(group_log, 'final cost is the best')
                 write_to_text_log(
-                    general_log, 'group {} has the new best score {}'.format(group_index, final_success_rate))
-                best_score = final_success_rate
+                    general_log, 'group {} has the new best cost {}'.format(group_index, final_cost))
+                best_cost = final_cost
                 best_config = current_config
                 copy_config(current_config, best_config_save_path)
