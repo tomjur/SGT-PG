19fba5a9c644df85b4fa77459211bd1814a4d907
tomjur <tomj@campus.technion.ac.il>
2019-10-01 11:15:48 +0300

    reduce std in higher levels at the same rate as level 1

--- sgt_online_rl_main_curriculum.py
+++ sgt_online_rl_main_curriculum.py
@@ -95,7 +95,7 @@ def run_for_config(config):
         use_value_functions = config['value_function']['use_value_functions']
 
         current_level = config['model']['starting_level']
-        global_step = 0
+        global_step, first_global_step_of_level = 0, 0
         best_cost, best_cost_global_step = None, None
         no_test_improvement, consecutive_learn_rate_decrease = 0, 0
 
@@ -121,7 +121,7 @@ def run_for_config(config):
 
             global_step = trainer.train_policy_at_level(current_level, global_step)
 
-            if global_step % config['policy']['decrease_std_every'] == 0:
+            if (global_step - first_global_step_of_level) % config['policy']['decrease_std_every'] == 0:
                 network.decrease_base_std(sess, current_level)
                 print_and_log('new base stds {}'.format(network.get_base_stds(sess, current_level)))
 
@@ -190,10 +190,11 @@ def run_for_config(config):
                     best_saver.restore(sess)
                     current_level += 1
                     if current_level <= config['model']['levels']:
+                        first_global_step_of_level = global_step
                         best_cost, best_cost_global_step = None, None
                         no_test_improvement, consecutive_learn_rate_decrease = 0, 0
-                        print_and_log('initiating level {} from previous level'.format(current_level))
                         if config['model']['init_from_lower_level']:
+                            print_and_log('initiating level {} from previous level'.format(current_level))
                             network.init_policy_from_lower_level(sess, current_level)
                     else:
                         # return the current level to its correct level for final prediction
