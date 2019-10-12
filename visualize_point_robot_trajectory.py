cda3cd0f4d0422e0d010a74c4daf7647c4672501
tomjur <tomj@campus.technion.ac.il>
2019-10-12 11:46:31 +0300

    point robot visualization working

--- visualize_point_robot_trajectory.py
+++ visualize_point_robot_trajectory.py
@@ -1,42 +1,62 @@
+import matplotlib.pyplot as plt
 import os
-import cPickle as pickle
-import glob
+import pickle
+import random
 
-from docker_path_helper import get_base_directory, init_dir
+from path_helper import get_base_directory, init_dir
 from point_robot_manager import PointRobotManager
 
-if __name__ == '__main__':
-    scenario = 'point_robot_hard_corridors'
-    # scenario = 'point_robot_easy'
-    model_name = '2019_05_15_12_03_55'
-    global_step = -1
+scenario = 'point_robot_box_0.2'
+# scenario = 'point_robot_box_0.8'
+# scenario = 'point_robot_easy2_transposed'
+# scenario = 'point_robot_easy2'
+# scenario = 'point_robot_hard_corridors'
 
-    def get_from_pickle(filepath):
-        with open(filepath, 'r') as f:
-            return pickle.load(f)
+model_name = '2019_10_10_14_44_02_to_remove'
+cycles = ['-1']
+source_trajectories = 'test_trajectories'
+num_trajectories = -1
+# num_trajectories = 1000
 
-    global_step = str(global_step)
-    paths_dir = os.path.join(get_base_directory(), scenario, 'trajectories', model_name, global_step)
-    path_files = [f for f in glob.glob(paths_dir + '/*.p') if '_motion_planner_' not in f]
 
-    image_dir = os.path.join('data', 'temp', model_name, global_step)
-    init_dir(image_dir)
+def parse_trajectory_line(line):
+    line = line.replace('[', '').replace(']', '').replace(os.linesep, '')
+    parts = [p for p in line.split(' ') if len(p) > 0]
+    assert len(parts) == 2
 
-    params_file = os.path.join(get_base_directory(), 'scenario_params', scenario, 'params.pkl')
-    point_robot_manager = PointRobotManager(get_from_pickle(params_file))
+    x1 = float(parts[0])
+    y1 = float(parts[1])
+    return x1, y1
 
-    for path_file in path_files:
-        input_filename = [part for part in path_file.split('/') if len(part) > 0][-1]
-        reference_path_file = os.path.join(paths_dir, input_filename.replace('_', '_motion_planner_'))
 
-        planned_path = get_from_pickle(path_file)
-        planned_path = [segment[0][0].tolist() for segment in planned_path] + [planned_path[-1][0][1].tolist()]
-        # planned_path = [p[0] for p in planned_path] + [planned_path[-1][1]]
-        motion_planner_path = get_from_pickle(reference_path_file)
+trajectories_dir = os.path.join(get_base_directory(), scenario, source_trajectories, model_name)
+output_dir = os.path.join(get_base_directory(), scenario, 'visualizations', source_trajectories, model_name)
 
-        f = point_robot_manager.plot([motion_planner_path, planned_path])
+obstacles_definitions_location = os.path.join(get_base_directory(), 'scenario_params', scenario, 'params.pkl')
+with open(obstacles_definitions_location, 'rb') as pickle_file:
+    obstacles_definitions = pickle.load(pickle_file)
 
-        output_filename = input_filename.replace('.p', '.png')
-        image_path = os.path.join(image_dir, output_filename)
-        f.savefig(image_path)
-        f.clear()
+for cycle in cycles:
+    cycle_output_dir = os.path.join(output_dir, cycle)
+    init_dir(cycle_output_dir)
+    point_robot_manager = PointRobotManager(obstacles_definitions)
+
+    trajectory_file = os.path.join(trajectories_dir, '{}.txt'.format(cycle))
+    with open(trajectory_file) as f:
+        all_lines = f.readlines()
+    all_lines = ''.join(all_lines)
+    data_by_path = all_lines.split('path_id_')
+    data_by_path = [d for d in data_by_path if len(d) > 0]
+    if 0 < num_trajectories < len(data_by_path):
+        # sample if the number of trajectories is bigger than zero and less than total number of trajectories
+        data_by_path = random.sample(data_by_path, num_trajectories)
+
+    for d in data_by_path:
+        lines = d.split(os.linesep)
+        path_id = lines[0]
+        status = lines[1]
+        trajectory = [parse_trajectory_line(l) for l in lines[2:] if len(l) > 0]
+        output_file_location = os.path.join(cycle_output_dir, '{}_{}.png'.format(path_id, status))
+        point_robot_manager.plot(paths=[trajectory]).savefig(output_file_location)
+        plt.clf()
+        print('printed path_id {} ({}), trajectory length {}'.format(path_id, status, len(trajectory)))
