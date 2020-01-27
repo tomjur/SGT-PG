746de96fdf2d28f1c0613a5a0615401c51509a06
tomjur <tomj@campus.technion.ac.il>
2020-01-27 12:41:58 +0200

    seq: add ability to repeat trajectories

--- episode_runner_sequential.py
+++ episode_runner_sequential.py
@@ -5,6 +5,7 @@ class EpisodeRunnerSequential:
         self.config = config
         self.game = game
         self.curriculum_coefficient = curriculum_coefficient
+        self.repeat_train_trajectories = self.config['model']['repeat_train_trajectories']
 
         self.fixed_start_goal_pairs = self.game.get_fixed_start_goal_pairs()
 
@@ -12,7 +13,18 @@ class EpisodeRunnerSequential:
         return self.play_episodes(self.fixed_start_goal_pairs, is_train)
 
     def play_random_episodes(self, number_of_episodes, is_train):
-        start_goal_pairs = self.game.get_free_start_goals(number_of_episodes, self.curriculum_coefficient)
+        number_of_start_goal_pairs = number_of_episodes
+        if is_train and self.repeat_train_trajectories is not None:
+            number_of_start_goal_pairs = -(-number_of_episodes // self.repeat_train_trajectories)  # does int division on with
+                                                                                           # ceiling instead of floor
+        start_goal_pairs = self.game.get_free_start_goals(number_of_start_goal_pairs, self.curriculum_coefficient)
+        if is_train and self.repeat_train_trajectories is not None:
+            start_goal_pairs_ = []
+            for _ in range(self.repeat_train_trajectories):
+                for s, g in start_goal_pairs:
+                    new_pair = (s.copy(), g.copy())
+                    start_goal_pairs_.append(new_pair)
+            start_goal_pairs = start_goal_pairs_[:number_of_episodes]
         return self.play_episodes(start_goal_pairs, is_train)
 
     def play_episodes(self, start_goal_pairs, is_train):
